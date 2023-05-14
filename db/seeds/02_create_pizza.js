7ef3b5e947c222c5b45920ab442303e65e60670a
James Hanna <James.Hanna87@gmail.com>
2023-05-14 02:52:21 -0500

    more example pizzas in seed

--- db/seeds/02_create_pizza.js
+++ db/seeds/02_create_pizza.js
@@ -16,6 +16,41 @@ exports.seed = function (knex) {
           ingredients: "Dough, Sauce, Cheese, Pepperoni",
           imgUrl: "https://i.imgur.com/286sM4Q.png",
         },
+        {
+          name: "Example_Pizza2",
+          price: 8.99,
+          description: "Example Description2",
+          ingredients: "Dough, Sauce, Cheese",
+          imgUrl: "https://i.imgur.com/286sM4Q.png",
+        },
+        {
+          name: "Example_Pizza3",
+          price: 10.99,
+          description: "Example Description3",
+          ingredients: "Dough, Sauce, Cheese, Pinapple, Ham",
+          imgUrl: "https://i.imgur.com/286sM4Q.png",
+        },
+        {
+          name: "Example_Pizza4",
+          price: 10.99,
+          description: "Example Description4",
+          ingredients: "Dough, Sauce, Cheese, Bacon, Pinapple",
+          imgUrl: "https://i.imgur.com/286sM4Q.png",
+        },
+        {
+          name: "Example_Pizza5",
+          price: 10.99,
+          description: "Example Description5",
+          ingredients: "Dough, Sauce, Cheese, Ham, Bacon, Pepperoni, Sausage",
+          imgUrl: "https://i.imgur.com/286sM4Q.png",
+        },
+        {
+          name: "Example_Pizza6",
+          price: 11.99,
+          description: "Example Description6",
+          ingredients: "Dough",
+          imgUrl: "https://i.imgur.com/286sM4Q.png",
+        },
       ]);
     });
 };
